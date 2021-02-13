c3d4e705e071d12eb387ee0ae4737ba121f31056
Oskar Šerelis <oskaras34@gmail.com>
2021-02-13 21:13:20 +0200

    initial BMI calculator project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
+import React, {useState, useEffect } from 'react';
 import './App.css';
+
+function BMIForm() {
+  const [height, setheight] = useState(0);
+  const [weight, setWeight] = useState(0);
+
+  return (
+    <main>
+      <form action="">
+        <h2>BMI calculator</h2>
+        <label>Weight
+          <input type="Number" placeholder="Enter weight" />
+        </label>
+        <label>Height (cm)
+        <input type="number" placeholder="Enter height"></input>
+        </label>
+      </form>
+    </main>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BMIForm></BMIForm>
     </div>
   );
 }
